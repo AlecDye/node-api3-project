024f4d196c9f49513909bd130c2981d3db6235d3
Alec Dye <alecdye@gmail.com>
2020-04-09 09:17:42 -0700

    Adding start script

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan")
+const helmet = require("helmet")
+
+
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
 
-//custom middleware
+// -- Middleware --
 
-function logger(req, res, next) {}
+server.use(logger)
+server.use(helmet())
+server.use(morgan("short"))
+server.use(express.json())
 
+// -- Custom Middleware Functions --
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} Request to ${req.originalUrl} ${req.get(
+      "Origin"
+      )}`
+      );
+      // tells express to cycle out of this function; browser will HANG if not present
+      next();
+    }
+    
+    //todo - build out these 3 middleware funcitons
+    function validateUserId()
+    
+    function validateUser()
+
+    function validatePost()
+    
+    // -- API Endpoints --
+    
+    server.get("/", (req, res) => {
+      res.send(`<h2>Let's write some middleware!</h2>`);
+    });
+  
+    //! This must be last
+    server.use(function(req, res) {
+      res.status(404).send("404 Error: Oops that page doesn't exist")
+    })
+    
 module.exports = server;
